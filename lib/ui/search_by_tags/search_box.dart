26f7a2800f653253e1e03fe8b7dd862c52a5d991
AvinashAgarwal14 <agarwalavinash14@gmail.com>
2020-01-17 19:36:41 +0530

    search box in tags setup

---
+++ lib/ui/search_by_tags/search_box.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SearchBox extends StatefulWidget {
+  @override
+  _SearchBoxState createState() => _SearchBoxState();
+}
+
+class _SearchBoxState extends State<SearchBox> {
+  @override
+  void setState(fn) {
+    if (mounted) {
+      super.setState(fn);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/ui/search_by_tags/tags.dart
+++ lib/ui/search_by_tags/tags.dart
@@ -7,6 +7,9 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import '../../model/event.dart';
 import '../../util/event_details.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'search_box.dart';
+
 
 Map<String, List<EventItem>> eventsByTags;
 
@@ -106,43 +109,74 @@ class _SearchByTagsState extends State<SearchByTags> {
         title: Text("Tags"),
       ),
       drawer: NavigationDrawer(),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 10.0),
-          ),
-          Container(
-              child: new Column(
-            mainAxisSize: MainAxisSize.min,
-            children: cardChildren,
-          )),
-          Divider(
-            color: Theme.of(context).brightness==Brightness.light ?Colors.grey:Color(0xFF505194),
-          ),
-          Container(
-              child: Expanded(
-            child: ListView.builder(
-                cacheExtent: MediaQuery.of(context).size.height*3,
-                itemCount: eventsByTags[_selectedTag].length,
-                itemBuilder: (context, position) {
-                  return Container(
-                      child: GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => new EventDetails(
-                                      item: eventsByTags[_selectedTag]
-                                          [position])),
-                            );
-                          },
-                          child: SearchByTagsCards(
-                              eventCard: eventsByTags[_selectedTag]
-                                  [position])));
-                }),
-          ))
-        ],
-      ),
+      body: Stack(
+          children: <Widget> [
+            Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                ),
+                Container(
+                    child: new Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: cardChildren,
+                    )),
+                Divider(
+                  color: Theme.of(context).brightness==Brightness.light ?Colors.grey:Color(0xFF505194),
+                ),
+                Container(
+                    child: Expanded(
+                      child: ListView.builder(
+                          cacheExtent: MediaQuery.of(context).size.height*3,
+                          itemCount: eventsByTags[_selectedTag].length,
+                          itemBuilder: (context, position) {
+                            return Container(
+                                child: GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => new EventDetails(
+                                                item: eventsByTags[_selectedTag]
+                                                [position])),
+                                      );
+                                    },
+                                    child: SearchByTagsCards(
+                                        eventCard: eventsByTags[_selectedTag]
+                                        [position])));
+                          }),
+                    ))
+              ],
+            ),
+            SlidingUpPanel(
+                minHeight: 65.0,
+                maxHeight: MediaQuery.of(context).size.height * 0.85,
+                panel: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(height: 5.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            width: 35,
+                            height: 8,
+                            decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                borderRadius:
+                                BorderRadius.all(Radius.circular(12.0))),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 13.0),
+                      Center(child: Text("Search")),
+                      SizedBox(height: 20.0),
+                      Container(
+                          padding: const EdgeInsets.only(left: 14.0, right: 14.0),
+                          height: MediaQuery.of(context).size.height * 0.75,
+                          child: SearchBox()),
+                    ]))
+          ]),
     );
   }
 
