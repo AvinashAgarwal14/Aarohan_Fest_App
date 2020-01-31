9d439204e0392c905115ee8ca5c169b6a9f40370
simran9928 <simranofficial99@gmail.com>
2020-02-01 01:43:48 +0530

    Contact us and SpalashScreen added

--- lib/ui/Intro.dart
+++ lib/ui/Intro.dart
@@ -1,6 +1,5 @@
 import 'package:intro_slider/intro_slider.dart';
 import 'dart:async';
-import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -19,29 +18,43 @@ class _SplashScreenState extends State<SplashScreen> {
     // TODO: implement initState
     super.initState();
     loadSavedData();
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarColor: Color(0xFF6D7650),
-        statusBarIconBrightness: Brightness.light,
-        systemNavigationBarIconBrightness: Brightness.dark));
 
-    Timer(Duration(milliseconds: 200), () {
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+
+    Timer(Duration(seconds: 4, milliseconds: 250), () {
       if (introScreen == null) {
         saveData();
-        Navigator.of(context)
-            .pushNamedAndRemoveUntil('/intro', (Route<dynamic> route) => false);
-      } else {
         Navigator.of(context).pushNamedAndRemoveUntil(
-            '/ui/account/login', (Route<dynamic> route) => false);
+              '/intro', (Route<dynamic> route) => false);
+      } else {
+          Navigator.of(context).pushNamedAndRemoveUntil(
+              '/ui/account/login', (Route<dynamic> route) => false);
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: CircularProgressIndicator(),
-    ));
+    return Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/SplashScreen.gif"), fit: BoxFit.fill)));
   }
 
   loadSavedData() async {
@@ -58,12 +71,15 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 }
 
+
+
 class IntroScreen extends StatefulWidget {
   @override
   _IntroScreenState createState() => _IntroScreenState();
 }
 
 class _IntroScreenState extends State<IntroScreen> {
+
   List<Slide> slides = new List();
 
   @override
@@ -86,18 +102,19 @@ class _IntroScreenState extends State<IntroScreen> {
         backgroundImage: "assets/INTRO3.png",
       ),
     );
+
   }
 
   void onDonePress() {
     // TODO: go to next screen
     Navigator.of(context).pushNamedAndRemoveUntil(
-        '/ui/account/login', (Route<dynamic> route) => false);
+                        '/ui/account/login', (Route<dynamic> route) => false);
   }
 
   void onSkipPress() {
     // TODO: go to next screen
     Navigator.of(context).pushNamedAndRemoveUntil(
-        '/ui/account/login', (Route<dynamic> route) => false);
+                        '/ui/account/login', (Route<dynamic> route) => false);
   }
 
   @override
