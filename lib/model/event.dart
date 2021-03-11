e2cd843da82189b5ba7938bdbde5618f3b194574
Souparno Paul <souparnopaul@gmail.com>
2021-03-11 16:35:25 +0530

    Changed UI of the Dashboard

--- lib/model/event.dart
+++ lib/model/event.dart
@@ -2,20 +2,21 @@ import 'package:firebase_database/firebase_database.dart';
 
 // A class where each Event is treated like an object
 // For simple usage and retrieval of data from FireBase
-class EventItem {
 
-  EventItem(
-      this.title,
-      this.body,
-      this.color,
-      this.imageUrl,
-      this.date,
-      this.category,
-      this.tag,
-      this.link,
-      this.location,
-      this.contact
-      );
+class EventResponse {
+  EventResponse(this.events);
+  List<EventItem> events;
+
+  EventResponse.fromJSON(List<dynamic> jsonList) {
+    events = jsonList.map((event) {
+      if (event != null) return EventItem.fromJSON(event);
+    }).toList();
+  }
+}
+
+class EventItem {
+  EventItem(this.title, this.body, this.color, this.imageUrl, this.date,
+      this.category, this.tag, this.link, this.location, this.contact);
 
   String key;
   String title;
@@ -29,17 +30,29 @@ class EventItem {
   String contact;
   String link;
 
-  EventItem.fromSnapshot(DataSnapshot snapshot) :
-        key = snapshot.key,
+  EventItem.fromSnapshot(DataSnapshot snapshot)
+      : key = snapshot.key,
         title = snapshot.value['title'],
         body = snapshot.value['body'],
         color = snapshot.value['color'],
         imageUrl = snapshot.value['imageUrl'],
-       //imageUrl="http://cdn.ebaumsworld.com/mediaFiles/picture/1961176/81660963.jpg",
+        //imageUrl="http://cdn.ebaumsworld.com/mediaFiles/picture/1961176/81660963.jpg",
         date = snapshot.value['date'],
         tag = snapshot.value['tag'],
         category = snapshot.value['category'],
         link = snapshot.value['link'],
         location = snapshot.value['location'],
         contact = snapshot.value['contact'];
+
+  EventItem.fromJSON(Map<String, dynamic> jsonMap)
+      : title = jsonMap['title'],
+        body = jsonMap['body'],
+        color = jsonMap['color'],
+        imageUrl = jsonMap['imageUrl'],
+        date = jsonMap['date'],
+        tag = jsonMap['tag'],
+        category = jsonMap['category'],
+        link = jsonMap['link'],
+        location = jsonMap['location'],
+        contact = jsonMap['contact'];
 }
--- lib/ui/dashboard/dashboard.dart
+++ lib/ui/dashboard/dashboard.dart
@@ -1,11 +1,16 @@
 import 'dart:async';
+import 'package:arhn_app_2021/model/date_model.dart';
+import 'package:arhn_app_2021/model/event.dart';
+import 'package:arhn_app_2021/model/event_type_model.dart';
+import 'package:arhn_app_2021/model/events_model.dart';
 import 'package:arhn_app_2021/ui/account/login.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:crypto/crypto.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter/services.dart';
+import 'package:flutter/services.dart' show SystemChrome, rootBundle;
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import '../../util/drawer.dart';
@@ -19,6 +24,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:arhn_app_2021/data/data.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -28,6 +34,13 @@ class Dashboard extends StatefulWidget {
 PageController controller;
 
 class _DashboardState extends State<Dashboard> {
+  List<DateModel> dates = new List<DateModel>();
+  List<EventTypeModel> eventsType = new List();
+  EventResponse res;
+  List<EventItem> events;
+  // List<EventsModel> events = new List<EventsModel>();
+  String todayDateIs = "12";
+
   bool darkThemeEnabled = false;
   FirebaseUser currentUser;
   int isEurekoinAlreadyRegistered;
@@ -54,6 +67,19 @@ class _DashboardState extends State<Dashboard> {
   @override
   void initState() {
     super.initState();
+
+    getJSON().then((value) {
+      print(json.decode(value));
+      res = EventResponse.fromJSON(json.decode(value));
+      setState(() {
+        events = res.events;
+      });
+    });
+
+    dates = getDates();
+    eventsType = getEventTypes();
+    // events = getEvents();
+
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
@@ -68,6 +94,10 @@ class _DashboardState extends State<Dashboard> {
     getUser();
   }
 
+  Future getJSON() {
+    return rootBundle.loadString('aarohan_events.json');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,13 +106,13 @@ class _DashboardState extends State<Dashboard> {
         child: AppBar(
           elevation: 0,
           brightness: Brightness.light,
-          backgroundColor: Colors.white,
+          backgroundColor: Colors.black,
           iconTheme: IconThemeData(
-            color: Colors.black,
+            color: Colors.white,
           ),
           textTheme: TextTheme(
             title: TextStyle(
-              color: Colors.black,
+              color: Colors.white,
               fontSize: 20.0,
             ),
           ),
@@ -90,12 +120,12 @@ class _DashboardState extends State<Dashboard> {
             "Aarohan",
             style: GoogleFonts.ubuntu(
               fontSize: 25,
-              color: Colors.black,
+              color: Colors.white,
             ),
           ),
           actions: <Widget>[
             IconButton(
-              color: Colors.black,
+              color: Colors.white,
               icon: Icon(Icons.youtube_searched_for),
               onPressed: () {
                 Navigator.of(context).pushNamed("/ui/tags");
@@ -136,63 +166,148 @@ class _DashboardState extends State<Dashboard> {
         ),
       ),
       drawer: NavigationDrawer(),
-      body: Stack(
-        children: <Widget>[
-          DashBoardLayout(),
-          SlidingUpPanel(
-            color: Colors.white,
-            minHeight: 65.0,
-            maxHeight: MediaQuery.of(context).size.height * 0.85,
-            panel: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(height: 5.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+      body: Container(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(color: Colors.black),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
+                    /// Dates
                     Container(
-                      width: 35,
-                      height: 8,
-                      decoration: BoxDecoration(
-                          color: Colors.grey[300],
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(12.0))),
-                    )
+                      height: 100,
+                      child: ListView.builder(
+                          padding: const EdgeInsets.symmetric(vertical: 20.0),
+                          itemCount: dates.length,
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return DateTile(
+                              weekDay: dates[index].weekDay,
+                              date: dates[index].date,
+                              isSelected: todayDateIs == dates[index].date,
+                            );
+                          }),
+                    ),
+
+                    /// Events
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Text(
+                      "All Events",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Container(
+                      height: 100,
+                      child: ListView.builder(
+                          itemCount: eventsType.length,
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return EventTile(
+                              imgAssetPath: eventsType[index].imgAssetPath,
+                              eventType: eventsType[index].eventType,
+                            );
+                          }),
+                    ),
+
+                    /// Popular Events
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Text(
+                      "Popular Events",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    Container(
+                      height: 300.0,
+                      child: events != null
+                          ? ListView.builder(
+                              itemCount: events.length,
+                              itemBuilder: (context, index) {
+                                return PopularEventTile(
+                                  desc: events[index].title,
+                                  imgeAssetPath: events[index].imageUrl,
+                                  date: events[index].date,
+                                  address: events[index].location,
+                                );
+                              },
+                            )
+                          : SizedBox(),
+                    ),
                   ],
                 ),
-                SizedBox(height: 13.0),
-                Center(
-                  child: Text(
-                    "Newsfeed",
-                    style: TextStyle(
-                      fontSize: 21.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey[800],
-                      // shadows: [
-                      //   BoxShadow(
-                      //       color: Colors.grey[800],
-                      //       offset: Offset(4.0, 4.0),
-                      //       blurRadius: 15.0,
-                      //       spreadRadius: 1.0),
-                      //   BoxShadow(
-                      //       color: Colors.white,
-                      //       offset: Offset(-4.0, -4.0),
-                      //       blurRadius: 15.0,
-                      //       spreadRadius: 1.0),
-                      // ],
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20.0),
-                Container(
-                    padding: const EdgeInsets.only(left: 14.0, right: 14.0),
-                    height: MediaQuery.of(context).size.height * 0.75,
-                    child: Newsfeed()),
-              ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
+      // body: Stack(
+      //   children: <Widget>[
+      //     DashBoardLayout(),
+      //     SlidingUpPanel(
+      //       color: Colors.white,
+      //       minHeight: 65.0,
+      //       maxHeight: MediaQuery.of(context).size.height * 0.85,
+      //       panel: Column(
+      //         crossAxisAlignment: CrossAxisAlignment.start,
+      //         children: <Widget>[
+      //           SizedBox(height: 5.0),
+      //           Row(
+      //             mainAxisAlignment: MainAxisAlignment.center,
+      //             children: <Widget>[
+      //               Container(
+      //                 width: 35,
+      //                 height: 8,
+      //                 decoration: BoxDecoration(
+      //                     color: Colors.grey[300],
+      //                     borderRadius:
+      //                         BorderRadius.all(Radius.circular(12.0))),
+      //               )
+      //             ],
+      //           ),
+      //           SizedBox(height: 13.0),
+      //           Center(
+      //             child: Text(
+      //               "Newsfeed",
+      //               style: TextStyle(
+      //                 fontSize: 21.0,
+      //                 fontWeight: FontWeight.bold,
+      //                 color: Colors.grey[800],
+      //                 // shadows: [
+      //                 //   BoxShadow(
+      //                 //       color: Colors.grey[800],
+      //                 //       offset: Offset(4.0, 4.0),
+      //                 //       blurRadius: 15.0,
+      //                 //       spreadRadius: 1.0),
+      //                 //   BoxShadow(
+      //                 //       color: Colors.white,
+      //                 //       offset: Offset(-4.0, -4.0),
+      //                 //       blurRadius: 15.0,
+      //                 //       spreadRadius: 1.0),
+      //                 // ],
+      //               ),
+      //             ),
+      //           ),
+      //           SizedBox(height: 20.0),
+      //           Container(
+      //               padding: const EdgeInsets.only(left: 14.0, right: 14.0),
+      //               height: MediaQuery.of(context).size.height * 0.75,
+      //               child: Newsfeed()),
+      //         ],
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 
@@ -326,5 +441,181 @@ class _DashboardState extends State<Dashboard> {
     print(response.body);
     var status = json.decode(response.body)['status'];
     return int.parse(status);
+  }
+}
+
+class DateTile extends StatelessWidget {
+  String weekDay;
+  String date;
+  bool isSelected;
+  DateTile({this.weekDay, this.date, this.isSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 20),
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+          color: isSelected ? Color(0xffffd530) : Color(0xff29404E),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: isSelected
+              ? [
+                  BoxShadow(
+                    color: Color(0xffFCCD00),
+                    spreadRadius: 4,
+                    blurRadius: 10,
+                  ),
+                  BoxShadow(
+                    color: Color(0xffFCCD00),
+                    spreadRadius: -4,
+                    blurRadius: 5,
+                  ),
+                ]
+              : []),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            date,
+            style: TextStyle(
+                color: isSelected ? Colors.black : Colors.white,
+                fontWeight: FontWeight.w600),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            weekDay,
+            style: TextStyle(
+                color: isSelected ? Colors.black : Colors.white,
+                fontWeight: FontWeight.w600),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class EventTile extends StatelessWidget {
+  String imgAssetPath;
+  String eventType;
+  EventTile({this.imgAssetPath, this.eventType});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      margin: EdgeInsets.only(right: 16),
+      decoration: BoxDecoration(
+          color: Color(0xff29404E), borderRadius: BorderRadius.circular(12)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            imgAssetPath,
+            height: 27,
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Text(
+            eventType,
+            style: TextStyle(color: Colors.white),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class PopularEventTile extends StatelessWidget {
+  String desc;
+  String date;
+  String address;
+  String imgeAssetPath;
+
+  /// later can be changed with imgUrl
+  PopularEventTile({this.address, this.date, this.imgeAssetPath, this.desc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      margin: EdgeInsets.only(bottom: 16),
+      decoration: BoxDecoration(
+          color: Color(0xff29404E), borderRadius: BorderRadius.circular(8)),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(left: 16),
+              width: MediaQuery.of(context).size.width - 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    desc,
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Image.asset(
+                        "assets/calender.png",
+                        height: 12,
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        date,
+                        style: TextStyle(color: Colors.white, fontSize: 10),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Image.asset(
+                        "assets/location.png",
+                        height: 12,
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        address,
+                        style: TextStyle(color: Colors.white, fontSize: 10),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ClipRRect(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(8),
+                  bottomRight: Radius.circular(8)),
+              child: Image.network(
+                imgeAssetPath,
+                errorBuilder: (context, error, stackTrace) {
+                  print(stackTrace);
+                  return SizedBox();
+                },
+                height: 100,
+                width: 120,
+                fit: BoxFit.cover,
+              )),
+        ],
+      ),
+    );
   }
 }
